6a30b69b8de4271aa4cc8345445fd55e8060549f
Alex Rosegrid <rosegrid58@gmail.com>
2026-04-03 12:30:50 +0530

    Fix the "remove duplicates" problem

--- leetcode/easy/src/unique_array.rs
+++ leetcode/easy/src/unique_array.rs
@@ -1,18 +1,11 @@
-pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-    let Some(max) = nums.iter().copied().max() else {
-        return 0;
-    };
-    let mut current: i32 = nums[0];
-    let mut k: i32 = 1;
-    // Replace all dups with max()
-    for x in nums.iter_mut().skip(1) {
-        if *x == current {
-            *x = max;
-        } else {
-            current = *x;
-            k += 1;
+pub fn remove_duplicates(nums: &mut [i32]) -> i32 {
+    let mut k = 1;
+    for i in 1..nums.len() {
+        // If this number is already at the correct position, do nothing
+        if nums[i] != nums[k - 1] {
+            nums[k] = nums[i];
+            k += 1
         }
     }
-    nums.sort();
-    k
+    k as i32
 }
