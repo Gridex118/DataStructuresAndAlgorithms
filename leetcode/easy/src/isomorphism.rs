51a1aeb018a1894bd104fc0931359d42a8a470da
Alex Rosegrid <rosegrid58@gmail.com>
2026-04-04 20:27:26 +0530

    Fix the "isomorphism" solution

--- leetcode/easy/src/isomorphism.rs
+++ leetcode/easy/src/isomorphism.rs
@@ -1,22 +1,14 @@
-use std::collections::HashMap;
-
 pub fn are_isomorphic(s: String, t: String) -> bool {
-    let s = s.chars().collect::<Vec<char>>();
-    let t = t.chars().collect::<Vec<char>>();
+    let s = s.as_bytes();
+    let t = t.as_bytes();
     if s.len() != t.len() { return false; }
-    let mut s_to_t: HashMap<char, char> = HashMap::new();
-    let mut t_to_s: HashMap<char, char> = HashMap::new();
-    for (i, &c) in s.iter().enumerate() {
-        if let Some(old) = s_to_t.insert(c, t[i])
-            && old != t[i] {
-                return false;
-            }
-    }
-    for (i, &c) in t.iter().enumerate() {
-        if let Some(old) = t_to_s.insert(c, s[i])
-            && old != s[i] {
-                return false;
-            }
+    let n = s.len();
+    let mut d1 = [0; 256];
+    let mut d2 = [0; 256];
+    for i in 0..n {
+        let (a, b) = (s[i] as usize, t[i] as usize);
+        if d1[a] != d2[b] { return false; }
+        (d1[a], d2[b]) = (i + 1, i + 1);
     }
     true
 }
