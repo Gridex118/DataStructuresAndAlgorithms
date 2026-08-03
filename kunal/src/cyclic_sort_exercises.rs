219bfb1861c7859217128239a093978bf02b2301
Alex Rosegrid <rosegrid58@gmail.com>
2026-08-03 23:24:26 +0530

    chore: Solve "find duplicated number" using cyclic sort

--- kunal/src/cyclic_sort_exercises.rs
+++ kunal/src/cyclic_sort_exercises.rs
@@ -41,6 +41,28 @@ pub fn find_disappeared_numbers(nums: &mut [i32]) -> Vec<i32> {
     dissappeared
 }
 
+/// Given n+1 `nums` in range [1, n], exactly one of which is duplicated.
+/// Find this duplicated number.
+///
+/// **Theory:** If number to be slotted is already positioned, this number is duplicated.
+pub fn find_duplicated(nums: &mut [i32]) -> i32 {
+    let n = nums.len();
+    let mut i = 0;
+    while i < n {
+        let j = nums[i] as usize - 1;
+        if i != j {
+            if nums[i] != nums[j] {
+                nums.swap(i, j);
+            } else {
+                return nums[i];
+            }
+        } else {
+            i += 1;
+        }
+    }
+    *nums.last().unwrap()
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -64,6 +86,17 @@ mod tests {
             (vec![1, 1], vec![2]),
         ] {
             assert_eq!(find_disappeared_numbers(&mut nums), dissapeared, "Failed for {nums:?}");
+        }
+    }
+
+    #[test]
+    fn test_find_duplicated_number() {
+        for (mut nums, duplicated) in [
+            (vec![1, 3, 4, 2, 2], 2),
+            (vec![3, 1, 3, 4, 2], 3),
+            (vec![2, 2], 2),
+        ] {
+            assert_eq!(find_duplicated(&mut nums), duplicated, "Failed for {nums:?}");
         }
     }
 
